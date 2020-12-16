092cbceae34d55257bcc4e56989d6d132af9bc7e
fanimator12 <torok.franciska.leonora@gmail.com>
2020-12-16 20:10:09 +0100

    removed some unnecessary stuff

--- FirstTier/Data/ArtworkService.cs
+++ FirstTier/Data/ArtworkService.cs
@@ -10,11 +10,11 @@ namespace FirstTier.Data
         Task<IList<Artwork>> GetArtworksAsync();
         Task DeleteArtworkAsync(int id);
         Task<Artwork> EditArtwork(Artwork artwork);
-        
-        //for searching
         Task<Artwork> GetArtworkAsync(int Id);
+        /*for searching
+        
         Task<int> CountAsync(string search);
         Task<List<Artwork>> ListAllAsync(int skip, int take, string orderBy, string direction, string search);
-
+*/
     }
 }
